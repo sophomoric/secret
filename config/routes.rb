d96dc0b74815574880cd7bc3ecae8499d916029c
Adrian Cann <adriantcann@gmail.com>
2015-07-19 19:58:19 -0400

    Root to page to make new secret

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pages#new"
   resources :secrets, as: :pages, controller: :pages, only: [:new, :create]
   get "/:url_key" => "permissions#new"
   resources :permissions, only: [:create]
