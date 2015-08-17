0b8d86a27edb1c362d0d8407a42539be874c3215
Adrian Cann <adriantcann@gmail.com>
2015-08-16 22:14:04 -0400

    WIP

--- app/assets/javascripts/helper.js
+++ app/assets/javascripts/helper.js
@@ -5,7 +5,28 @@ var _h = (function(){
     $target.val(newValue).keyup();
   };
 
+  var createMenu = function($menu){
+    $menu.click(function(e){
+      e.preventDefault();
+      var $item = $(e.target);
+      _selectFromSiblings($item, "active");
+
+      var selector = $item.css("class");
+      var matcher = selector + "-input";
+      var input = $(matcher);
+      _selectFromSiblings($item, "hidden");
+    });
+  };
+
+  var _selectFromSiblings = function($element, className){
+    $element.toggleClass("active");
+    $.each($element.siblings(), function(i, el){
+    $(el).removeClass("active");
+    });
+  };
+
   return {
-    appendText: appendText
+    appendText: appendText,
+    createMenu: createMenu
   };
 })();
--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,11 +1,5 @@
 $(function(){
   var $menu = $(".menu");
 
-  $menu.click(function(e){
-    e.preventDefault();
-    $button = $(e.target);
-    menu_selector = $button.attr("class");
-    $menu_selector = $(menu_selector);
-    debugger
-  });
+  _h.createMenu($menu);
 });
