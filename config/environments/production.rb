b4c50e8b9dd72311069cbff3c5ddd8962b74f80e
Adrian Cann <adriantcann@gmail.com>
2016-06-04 13:13:57 -0400

    Prevent message from showing up in logs
    * This application could be used to send sensitive information. The
      message portion should not show up in application logs since that
      would leave a record the sensitive information.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  config.filter_parameters << :message
 end
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
