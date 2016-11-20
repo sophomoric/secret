527a7f1f719ad8f9aed68377d385374c38b920fc
Adrian Cann <adriantcann@gmail.com>
2016-11-20 16:50:30 -0500

    Apply OO approach to urlKey preview logic
    * Travis started failing for no apparent reason, but this seemed a
    chance to use a named object rather than a global collection of
    functions.
    * The logic for splitting on the last `/` could probably be taken out
    if this were to be a `js.erb` template and the host name could be
    interpolated and set to the `this.placeholder` value

--- app/assets/javascripts/url_key.js
+++ app/assets/javascripts/url_key.js
@@ -1,16 +1,22 @@
 $(function(){
-  var $input = $("#page_url_key");
-  var $previewLabel = $(".page_url_key label");
-  var previewPlaceholder = $previewLabel.text();
+  var UrlKey = function() {
+    this.$input = $("#page_url_key");
+    this.$previewLabel = $(".page_url_key label");
+    this.placeholder = this.$previewLabel.text();
 
-  Functions.modifyLabelText = function modifyLabelText(currentText, urlKey) {
-    var split = currentText.split("/");
-    var oldText = split.slice(0, split.length - 1);
-    return oldText.join("/") + "/" + encodeURI(urlKey);
+    this.modifyLabelText = function modifyLabelText(currentText, key) {
+      var split = currentText.split("/");
+      var oldText = split.slice(0, split.length - 1);
+      return oldText.join("/") + "/" + encodeURI(key);
+    };
+
+    this.$input.keyup(function(){
+      var currentInput = this.$input.val();
+      var labelText = this.modifyLabelText(this.placeholder, currentInput);
+      this.$previewLabel.text(labelText);
+    });
   };
 
-  $input.keyup(function(){
-    var labelText = Functions.modifyLabelText(previewPlaceholder, $input.val());
-    $previewLabel.text(labelText);
-  });
+  window.UrlKey = UrlKey;
+  window.urlKey = new UrlKey();
 });
--- spec/javascripts/url_key_spec.js
+++ spec/javascripts/url_key_spec.js
@@ -1,25 +1,29 @@
 //= require url_key
 
-describe("Url Key", function() {
-  describe("modifyLabelText", function() {
-    it("finds a url and swaps in a urlKey", function() {
-      var currentText = "Specify url: http://onetimenote.com/example";
-      var urlKey = "my-page";
+$(function(){
+  describe("UrlKey", function() {
+    describe("modifyLabelText", function() {
+      it("finds a url and swaps in a urlKey", function() {
+        var currentText = "Specify url: http://onetimenote.com/example";
+        var key = "my-page";
 
-      var result = Functions.modifyLabelText(currentText, urlKey);
+        var urlKey = new window.UrlKey();
+        var result = urlKey.modifyLabelText(currentText, key);
 
-      var expectedResult = "Specify url: http://onetimenote.com/my-page";
-      expect(result).toEqual(expectedResult);
-    });
+        var expectedResult = "Specify url: http://onetimenote.com/my-page";
+        expect(result).toEqual(expectedResult);
+      });
 
-    it("URI encodes spaces", function() {
-      var currentText = "Specify url: http://onetimenote.com/example";
-      var urlKey = "in between";
+      it("URI encodes spaces", function() {
+        var currentText = "Specify url: http://onetimenote.com/example";
+        var key = "in between";
 
-      var result = Functions.modifyLabelText(currentText, urlKey);
+        var urlKey = new window.UrlKey();
+        var result = urlKey.modifyLabelText(currentText, key);
 
-      var expectedResult = "Specify url: http://onetimenote.com/in%20between";
-      expect(result).toEqual(expectedResult);
+        var expectedResult = "Specify url: http://onetimenote.com/in%20between";
+        expect(result).toEqual(expectedResult);
+      });
     });
   });
 });
