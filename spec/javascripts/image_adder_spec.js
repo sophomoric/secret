f80ba8e53753e4f465e0b1f26f8580be8ca3e24b
Adrian Cann <adriantcann@gmail.com>
2016-11-20 17:23:48 -0500

    Have js specs run after document loads
    * Counter and ImageAdder both interact with the DOM
    * The constructors themselves are wrapped in a `$(function(){});` but
    the tests may require DOM elements to be created in test.

--- spec/javascripts/image_adder_spec.js
+++ spec/javascripts/image_adder_spec.js
@@ -1,26 +1,28 @@
 //= require image_adder
 
-describe("ImageAdder", function() {
-  describe("clicking the 'use' button", function() {
-    it("inserts a placeholder on the page imagekey0", function() {
-      var $useButton = $("<button class='use'></button>");
-      var $pageMessage = $("<textarea id='page_message'></textarea>");
-      $("body").append($useButton).append($pageMessage);
-      stubCurrentImg("url.currentImg.gif");
+$(function(){
+  describe("ImageAdder", function() {
+    describe("clicking the 'use' button", function() {
+      it("inserts a placeholder on the page imagekey0", function() {
+        var $useButton = $("<button class='use'></button>");
+        var $pageMessage = $("<textarea id='page_message'></textarea>");
+        $("body").append($useButton).append($pageMessage);
+        stubCurrentImg("url.currentImg.gif");
 
-      window.imageAdder = new ImageAdder();
-      $useButton.trigger("click");
+        window.imageAdder = new ImageAdder();
+        $useButton.trigger("click");
 
-      expect($pageMessage.val()).toEqual(" imgkey0 ");
-      expect(imageAdder.imageMap[0]).toEqual("url.currentImg.gif");
+        expect($pageMessage.val()).toEqual(" imgkey0 ");
+        expect(imageAdder.imageMap[0]).toEqual("url.currentImg.gif");
+      });
     });
-  });
 
-  function stubCurrentImg(returnVal){
-    window.counter = {
-      currentImg: function currentImg(){
-        return returnVal;
-      }
-    };
-  }
+    function stubCurrentImg(returnVal){
+      window.counter = {
+        currentImg: function currentImg(){
+          return returnVal;
+        }
+      };
+    }
+  });
 });
