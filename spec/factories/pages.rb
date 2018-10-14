c85d2d12cdffc91d782084fb13946aef424397c3
Adrian Cann <adriantcann@gmail.com>
2018-10-14 11:17:49 -0400

    Upgraded to factory_bot_rails
    * There was a deprecation in the logs about factory_girl_rails
    * The gem factory_girl_rails has been replaced with factory_bot_rails
    * There are some nice scripts to run for find/replace:
    https://github.com/thoughtbot/factory_bot/blob/4-9-0-stable/UPGRADE_FROM_FACTORY_GIRL.md
    * Also some slight changes in method signatures

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,8 +1,8 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :page do
-    password "Password"
-    duration 3
-    message "Defeat Rebulba"
-    expires_at 1.week.from_now
+    password { "Password" }
+    duration { 3 }
+    message { "Defeat Rebulba" }
+    expires_at { 1.week.from_now }
   end
 end
