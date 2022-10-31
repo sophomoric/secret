05424f69722f905312eb3f101d0a542e050ee0ef
Adrian Cann <adriantcann@gmail.com>
2022-10-31 17:49:13 -0400

    Fix feature specs locally
    ***Why***
    * Feature specs no longer pass locally (been a while)
    
    ***The changes***
    * The chromedriver-helper gem has been deprecated in favor of the
      webdrivers gem: https://github.com/flavorjones/chromedriver-helper
    * Apparently we need to call the googleapis https://chromedriver.storage.googleapis.com/LATEST_RELEASE_106.0.5249
      and then download a package the first time we run this (but later we
      can disallow web connections in test)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,4 +28,10 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+# for localhost but also webdriver that calls googleapis:
+# e.g. https://chromedriver.storage.googleapis.com/LATEST_RELEASE_106.0.5249
+# Then downloads the corresponding package when you run your specs with the
+# following line commented in:
+# WebMock.allow_net_connect!
+# and the line below commented out:
 WebMock.disable_net_connect!(allow_localhost: true)
